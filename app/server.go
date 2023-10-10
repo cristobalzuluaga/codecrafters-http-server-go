5785fe47fe8c27c33456b3d61deb650258725a60
cloudcraftman <cristobalzuluagas@gmail.com>
2023-10-10 14:25:25 -0500

    pass 3nd stage

--- app/server.go
+++ app/server.go
@@ -2,41 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"os"
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	fmt.Println("Listening on 0.0.0.0:4221")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleRequest(conn)
 	}
+}
+
+func handleRequest(conn net.Conn) {
 	defer conn.Close()
 
-	var req []byte
+	readBuffer := make([]byte, 1024)
 
-	_, err = conn.Read(req)
+	_, err := conn.Read(readBuffer)
 	if err != nil {
-		log.Println("Error reading bytes: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error reading: ", err.Error())
+		return
 	}
+	fmt.Println("Received: ", string(readBuffer))
 
-	res, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		log.Println("Error writing bytes: ", err.Error())
-		os.Exit(1)
-	}
-
-	fmt.Printf("res: %v\n", res)
-	// fmt.Printf("req: %v\n", req)
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
